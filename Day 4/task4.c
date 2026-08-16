c8bab7e6afe1f5186a36e84125b949615d39e0c3
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2026-08-16 09:15:12 +0530

    day 4

---
+++ Day 4/task4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+   
+    for (int a = 0; a <= 7; a++)
+   {
+    for(int i = a; i <= 7; i++) {
+        printf(" * ");
+        
+      }
+      printf("\n");
+   }
+   
+    }
